120eb6a8593e12450bacf71f4084c0f3bb98f108
Anand <anand@Anands-MacBook-Pro.local>
2025-04-10 13:46:41 +0530

    ui changes

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -7,24 +7,26 @@ const TableComponent = ({ colums, data, tableTitle }) => {
       <table className="min-w-full border-collapse border border-gray-300">
         <thead>
           <tr className="bg-gray-200">
-            {colums.map((name, index) => (
-              <th key={index} className="border border-gray-300 p-2">
-                {name}
-              </th>
-            ))}
+            {colums &&
+              colums.map((name, index) => (
+                <th key={index} className="border border-gray-300 p-2">
+                  {name}
+                </th>
+              ))}
           </tr>
         </thead>
 
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index} className="text-center">
-              {Object.values(item).map((key, i) => (
-                <td key={i} className="border border-gray-300 p-2">
-                  {key}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {data &&
+            data.map((item, index) => (
+              <tr key={index} className="text-center">
+                {Object.values(item).map((key, i) => (
+                  <td key={i} className="border border-gray-300 p-2">
+                    {key}
+                  </td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,8 @@
 const ENV = "DEV";
 
 export const ALL_API = {
-  DEV_API_HOST: "http://182.77.59.45:5010",
+  // DEV_API_HOST: "http://182.77.59.45:5010",
+  DEV_API_HOST: "http://122.160.157.197:5010",
 };
 
 export const CONFIG = {
