ee273b6c1ee9c26345de03f0684de92e40cb14c9
Anand <anand@Anands-MacBook-Pro.local>
2025-06-09 14:47:33 +0530

    logo added

--- src/components/WithoutHeaderLayout.jsx
+++ src/components/WithoutHeaderLayout.jsx
@@ -4,7 +4,8 @@ import Footer from "./Footer";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../src/images/logo.png";
 import RAILWAY_CONST from "../utils/RailwayConst"
-
+import railPlotLogo from "../../src/images/railPlotLogo.png";
+import logoIconRailPlot from "../../src/images/logoIconRailPlot.png";
 
 const WithoutHeaderLayout = () => {
   return (
@@ -13,10 +14,31 @@ const WithoutHeaderLayout = () => {
       <div className="max-w-[1300px] mx-auto flex flex-row justify-between w-full">
         {/* Logo */}
         <div className="h-[90px] logoHeader">
-          <Link to={RAILWAY_CONST.ROUTE.HOME}>
-            <img className="logo" src={logo} alt="Logo" />
-          </Link>
-        </div>
+              <Link
+                to={
+                  RAILWAY_CONST.ROUTE.HOME
+                }
+              >
+              
+                <span className="flex flex-row">
+                  <img
+                    src={logoIconRailPlot}
+                    alt="logo"
+                    className="h-[44px] mr-[4px]"
+                  />
+                  <span>
+                    <img
+                      src={railPlotLogo}
+                      alt="logo"
+                      className="h-[30px] mt-[2px] mb-[-3px]"
+                    />
+                    <span className=" text-[10px] ml-[1px]">
+                      Visualize. Analyze. Optimize.
+                    </span>
+                  </span>
+                </span>
+              </Link>
+            </div>
        
       </div>
     </header>
