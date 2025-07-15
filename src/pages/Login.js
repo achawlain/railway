fe150a2df642491883d6d5234fee5bccbdde36a9
Anand <anand@Anands-MacBook-Pro.local>
2025-07-15 15:24:01 +0530

    login changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -74,14 +74,14 @@ const Login = () => {
         // userObj.access_token =
         //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0NzEyMDQ1NCwianRpIjoiZDlhZDBmYjMtYWE0My00ZGY2LWIxYmItOGI1ZWJkZTU4MDFmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImdpcmlzaCIsIm5iZiI6MTc0NzEyMDQ1NCwiY3NyZiI6IjI1MTE4NTE5LTViMTYtNDJjMC1iYTRjLTMyNWE5OTE1YWE4MiIsImV4cCI6MTc0Nzk4NDQ1NCwidV9pZCI6IjEiLCJuYW1lIjoiR2lyaXNoIEt1bWFyIiwicm9sZSI6MSwiZGVzaWduYXRpb24iOiJUZWNoIn0.oMH-bwOglul8MLmFQJ6OxG8UbYudPELZgjAkA3ErtOs";
         setDataOnLocalStorage("userInfo", userObj);
+        console.log("access_token-----", userObj.access_token);
+        console.log("type of", typeof userObj.access_token);
 
         if (
           window.AndroidBridge &&
           typeof window.AndroidBridge.receiveAccessToken === "function"
         ) {
-          window.AndroidBridge?.receiveAccessToken(
-            localStorage.getItem(userObj.access_token)
-          );
+          window.AndroidBridge?.receiveAccessToken(userObj.access_token);
         }
 
         if (params.get("dataCollection")) {
