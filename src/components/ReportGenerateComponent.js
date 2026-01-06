b0f0e87cda319f614e15bbe9b0991ea3ac724eab
umangvarshney19 <umang.varshney@orryworx.com>
2026-01-06 19:39:34 +0530

    getting one report detail

--- src/components/ReportGenerateComponent.js
+++ src/components/ReportGenerateComponent.js
@@ -145,26 +145,43 @@ const ReportGenerateComponent = () => {
   // };
 
   useEffect(() => {
+    // const fetchReportById = async () => {
+    //   setLoading(true);
+    //   try {
+    //     const response = await apiService("get", RAILWAY_CONST.API_ENDPOINT.REPORTS_SLASH);
+    //     // console.log("Response Data:", response.data);
+    //     const reports = Array.isArray(response.data)
+    //       ? response.data
+    //       : response.reports || [];
+    //     // console.log("Reports:", reports,reports.id, id);
+
+    //     const matchedReport = reports.find((report) => String(report?.id) === String(id));
+    //     // console.log("Matched Report:", matchedReport);
+    //     if (matchedReport) {
+    //       setCurrentReport(matchedReport);
+    //     } else {
+    //       console.warn("No report found with id:", id);
+    //       setCurrentReport(null); // Or handle gracefully
+    //     }
+    //   } catch (error) {
+    //     console.error("Error fetching reports:", error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // };
     const fetchReportById = async () => {
       setLoading(true);
       try {
-        const response = await apiService("get", RAILWAY_CONST.API_ENDPOINT.REPORTS_SLASH);
-        // console.log("Response Data:", response.data);
-        const reports = Array.isArray(response.data)
-          ? response.data
-          : response.reports || [];
-        // console.log("Reports:", reports,reports.id, id);
-
-        const matchedReport = reports.find((report) => String(report?.id) === String(id));
-        // console.log("Matched Report:", matchedReport);
-        if (matchedReport) {
-          setCurrentReport(matchedReport);
-        } else {
-          console.warn("No report found with id:", id);
-          setCurrentReport(null); // Or handle gracefully
-        }
+        const response = await apiService(
+          "get",
+          `${RAILWAY_CONST.API_ENDPOINT.REPORTS_SLASH}${id}`
+        );
+    
+        setCurrentReport(response.data);
+    
       } catch (error) {
-        console.error("Error fetching reports:", error);
+        console.error("Error fetching report:", error);
+        setCurrentReport(null);
       } finally {
         setLoading(false);
       }
