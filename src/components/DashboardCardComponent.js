9d132669c60d37ad007a3a8439444a966c7d434d
umangvarshney19 <umang.varshney@orryworx.com>
2026-01-23 03:39:21 +0530

    reports in whatsapp

--- src/components/DashboardCardComponent.js
+++ src/components/DashboardCardComponent.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import deleteIcon from "../images/delete-icon.svg";
 import downloadIcon from "../images/downloadIcon.svg";
+import whatsappIcon from "../images/whatsapp.png";
 import viewIcon from "../images/viewIcon.svg";
 import sourceFileIcon from "../images/file.png";
 import { useNavigate } from "react-router-dom";
@@ -292,6 +293,20 @@ const DashboardCardComponent = ({ item, onDelete, onView, refreshReports }) => {
           />
           Download
         </a>
+        <a
+          href={`${baseUrl}/${RAILWAY_CONST.API_ENDPOINT.REPORTS}/whatsapp_report/${item.id}?from_station=${item.stn_from}&to_station=${item.stn_to}&jwt=${userInfo.access_token || ""}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex w-[50%] text-[13px] opacity-[.8] text-[#414141] items-center cursor-pointer flex-col hover:opacity-[1] justify-center text-center leading-[13px]"
+        >
+          <img
+            alt="view icon"
+            src={whatsappIcon}
+            className="cursor-pointer leading-[13px] w-[22px] mr-[2px] mb-1"
+          />
+          Whatsapp
+        </a>
+
       </div>
       {showConfirm && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
